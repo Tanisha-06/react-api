3f8e2238b56a7790a8f9513051d5ee62277cb1f5
Tanisha Pandey <2019160@iiitdmj.ac.in>
2021-06-27 23:10:36 +0530

    second-commit

--- src/components/PersonalData.js
+++ src/components/PersonalData.js
@@ -8,10 +8,9 @@ import PublicIcon from '@material-ui/icons/Public';
 import WcIcon from '@material-ui/icons/Wc';
 import PersonIcon from '@material-ui/icons/Person';
 const PersonalData = (props) => {
-console.log(props.data);
   return (
     <div className={classes.card}>
-        <div style={{textAlign:'center'}}> <img src={props.data.picture.thumbnail} className={classes.photo} alt="image"/></div>
+        <div style={{textAlign:'center'}}> <img src={props.data.picture.thumbnail} className={classes.photo}/></div>
         <div>
          <h3 className={classes.item} style={{textAlign:'center',fontFamily:'inherit',justifyContent:'center'}}><PersonIcon/>{props.data.title} {props.data.fname} {props.data.lname}</h3>
          <h5 className={classes.item}><WcIcon/>{props.data.gender}</h5>
